5a1398ec31a477178cdab14ba0440844f22e5dc7
Sarthak <sarthakstha24@gmail.com>
2023-10-15 13:16:07 +0545

    Main

---
+++ index.js
@@ -0,0 +1,29 @@
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+  .prompt([
+    {
+      message: "Type in your URL: ",
+      name: "URL",
+    },
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qrImage.png"));
+
+    fs.writeFile("URL.txt", url, (err) =>{
+      if(err) throw err;
+      console.log("File has been saved");
+    })
+
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      console.log(error);
+    } else {
+      console.log("External Error");
+    }
+  });
